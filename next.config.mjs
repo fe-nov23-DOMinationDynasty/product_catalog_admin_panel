33edd798fea3900caddf930cca8a87f901f76b9a
Oleksandr Boiko <iccc97@gmail.com>
2024-03-20 12:29:23 +0200

    add template

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const nextConfig = {
+  trailingSlash: true,
+  reactStrictMode: false,
+  experimental: {
+    esmExternals: false,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      apexcharts: path.resolve(
+        __dirname,
+        './node_modules/apexcharts-clevision'
+      ),
+    };
+
+    return config;
+  },
+};
 
 export default nextConfig;
